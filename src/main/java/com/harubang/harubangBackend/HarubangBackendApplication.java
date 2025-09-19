0f0ae6e9e881a89d06eed81d093de58a46825216
JeongAreum <jungarum0607@gmail.com>
2025-09-19 22:12:45 +0900

    chore: initial commit for harubangBackend

---
+++ src/main/java/com/harubang/harubangBackend/HarubangBackendApplication.java
@@ -0,0 +1,13 @@
+package com.harubang.harubangBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarubangBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarubangBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harubang/harubangBackend/HarubangBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harubang.harubangBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarubangBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
